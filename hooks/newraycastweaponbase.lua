f4b564cb67a0a19d039844ea3bd87c95f4fca4e5
0x008Y1E00 <0x008Y1E00@gmail.com>
2025-08-31 14:55:03 +0500

    Version 1.2

--- hooks/newraycastweaponbase.lua
+++ hooks/newraycastweaponbase.lua
@@ -1,15 +1,17 @@
-local old_weapon_tweak_data = NewRaycastWeaponBase.weapon_tweak_data
-
-function NewRaycastWeaponBase:weapon_tweak_data(...)
-    local wpn_twk_data = old_weapon_tweak_data(self, ...)
+local function nrwb_wtd_liberator(self)
+    local wpn_twk_data = Hooks:GetReturn()
 
     if self._name_id then
         if self._name_id == "contraband" then
             wpn_twk_data.use_stance = "contraband"
         elseif self._name_id == "liberator" then
             wpn_twk_data.use_stance = "liberator"
+        elseif self._name_id == "contraband_m203" then
+            wpn_twk_data.use_stance = "liberator"
         end
     end
 
     return wpn_twk_data
 end
+
+Hooks:PostHook(NewRaycastWeaponBase, "weapon_tweak_data", "psthk_nrwb_wtd_liberator", nrwb_wtd_liberator)
